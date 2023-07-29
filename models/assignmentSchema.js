b68eebe4a4842fb9390a05f1252ffc08ce695974
rayhan mujumdar <rayhanmujumdar0177@gmail.com>
2023-07-29 16:52:01 +0600

    assigmentSchema added

---
+++ models/assignmentSchema.js
@@ -0,0 +1,26 @@
+const {Schema,model} = require('mongoose')
+
+const assignmentSchema = new Schema({
+    id: Schema.Types.ObjectId,
+    title: {
+        type: String,
+        required: [true,"Title must be required"]
+    },
+    videoId: {
+        type: Schema.Types.ObjectId,
+        ref: "Video",
+        required: [true,"VideoId must be required"]
+    },
+    totalMark:{
+        type: Number,
+        enum: {
+            values: [100,200,300],
+            message: `{VALUE} is not supported`
+        },
+        required: [true,"Assignment Number must be required"]
+    }
+})
+
+const Assignment = model("Assignment",assignmentSchema)
+
+module.exports = Assignment
