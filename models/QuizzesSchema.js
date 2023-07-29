13688a4717c58bf2824623312607a67211fdb9ab
rayhan mujumdar <rayhanmujumdar0177@gmail.com>
2023-07-29 16:52:26 +0600

    quizzesSchema added

---
+++ models/QuizzesSchema.js
@@ -0,0 +1,26 @@
+const { Schema, model } = require("mongoose");
+
+const QuizzesSchema = new Schema({
+  id: Schema.Types.ObjectId,
+  question: {
+    type: String,
+    required: [true, "Question must be required"],
+  },
+  videoId: {
+    type: Schema.Types.ObjectId,
+    ref: "Video",
+  },
+  options: [
+    new Schema({
+      id: Schema.Types.ObjectId,
+      option: {
+        type: String,
+        require: [true, "Option must be required"],
+      },
+      isCorrect: Boolean,
+    }),
+  ],
+});
+
+const Quizzes = model("Quizzes", QuizzesSchema);
+module.exports = Quizzes;
