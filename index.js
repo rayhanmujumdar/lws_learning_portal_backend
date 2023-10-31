cb3b53b5f666ba3246c1a1fdf8abc46486f5b64d
rayhan mujumdar <rayhanmujumdar0177@gmail.com>
2023-11-01 01:50:43 +0600

    mongodb cloud database url added

--- index.js
+++ index.js
@@ -6,7 +6,9 @@ const http = require("http");
 const server = http.createServer(app);
 const PORT = process.env.PORT || 4000;
 
-db(process.env.DB_URL).then(() => {
+db(
+  `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASS}@cluster0.izepew7.mongodb.net/course`
+).then(() => {
   server.listen(PORT, () => {
     console.log(`Server listening port is ${PORT}`);
   });
